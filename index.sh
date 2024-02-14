528201671f80a248552183dc0cfb3988ac8b9296
DjKesu <km67000@yahoo.com>
2024-02-14 16:34:11 -0500

    added first report

--- index.sh
+++ index.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd ./reports
-echo "<html><body><h1>PowerTOP Reports</h1><ul>" > index.html
+echo "<html><body><h1>PowerTOP Reports</h1><ul>" > ../index.html
 for report in $(ls *.html | sort -r); do
-  echo "<li><a href=\"$report\">$report</a></li>" >> index.html
+  echo "<li><a href=\"$report\">$report</a></li>" >> ../index.html
 done
 echo "</ul></body></html>" >> index.html
