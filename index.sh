8f9386fd06c0fed2710bddd98ccae5e27a65f4a2
DjKesu <km67000@yahoo.com>
2024-02-14 16:42:41 -0500

    fixed some issues with directories

--- index.sh
+++ index.sh
@@ -3,6 +3,6 @@
 cd ./reports
 echo "<html><body><h1>PowerTOP Reports</h1><ul>" > ../index.html
 for report in $(ls *.html | sort -r); do
-  echo "<li><a href=\"$report\">$report</a></li>" >> ../index.html
+  echo "<li><a href=/reports/\"$report\">$report</a></li>" >> ../index.html
 done
 echo "</ul></body></html>" >> index.html
